2eb3a4a91c49648e75cbd5821b0edbf7828b20ce
Jeysel Pacheco Bastos <jeysel@gmail.com>
2023-03-02 17:40:18 -0300

    Alteração de rotas

---
+++ controllers/membro_controller.py
@@ -0,0 +1,63 @@
+from fastapi.requests import Request
+from fastapi import UploadFile
+from aiofile import async_open
+from uuid import uuid4
+from core.configs import settings
+from core.database import get_session
+from models.membro_model import MembroModel
+from controllers.base_controller import BaseController
+
+class MembroController(BaseController):
+    def __int__(self, request: Request) -> None:
+        super().__init__(request, MembroModel)
+
+    async def post_crud(self) -> None:
+        # Recebe dados do form
+        form = await self.request.form()
+
+        nome: str = form.get('nome')
+        funcao: str = form.get('funcao')
+        imagem: UploadFile = form.get('imagem')
+
+        # Gera Nome aletório para imagem
+        arquivo_ext: str = imagem.filename.split('.')[-1]
+        novo_nome: str = f"{str(uuid4())}.{arquivo_ext}"
+
+        # Instanciar o objeto
+        membro: MembroModel = MembroModel(nome=nome, funcao=funcao, imagem=novo_nome)
+
+        # Fazer o upload do arquivo
+        async with async_open(f"{settings.MEDIA}/{novo_nome}", "wb") as afile:
+            await afile.write(imagem.file.read())
+
+        # Cria a sessão e insere no banco de dados
+        async with get_session() as session:
+            session.ad(membro)
+            await session.commit()
+
+    async def put_crud(self, obj: object) -> None:
+        raise with get_session() as session:
+            membro: MembroModel = await session.get(self.model, obj.id)
+
+            if membro:
+                #Recebe os dados do form
+                form = await self.request.form()
+
+                nome: str = form.get('nome')
+                funcao: str = form.get('funcao')
+                imagem: UploadFile = form.get('imagem')
+
+                if nome and nome != membro.nome:
+                    membro.nome = nome
+                if funcao and funcao != membro.funcao:
+                    membro.funcao = funcao
+                if imagem.filename:
+                    # Gera um nome aletório
+                    arquivo_ext: str = imagem.filename.split('.') [-1]
+                    novo_nome: str = f"{str(uuid4())}.{arquivo_ext}"
+                    membro.imagem = novo_nome
+                    # Faz o upload da imagem
+                    async with async_open(f"{settings.MEDIA}/{novo_nome}", "wb") as afile:
+                        await afile.write(imagem.file.read())
+                session.add(membro)
+                await session.commit()
---
+++ views/admin/membro_admin.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+from fastapi.routing import APIRouter
+from starlette.routing import Route
+from fastapi import status
+from fastapi.requests import Request
+from fastapi.responses import Response, RedirectResponse
+from fastapi.exceptions import HTTPException
+from core.configs import settings
+from controllers.membro_controller import MembroController
+from views.admin.base_crud_view import BaseCrudView
+
+class MembroAdmin(BaseCrudView):
+
+    def __init__(self) -> None:
+        self.router = APIRouter()
+
+        self.router.routes.append(Route(path='/membro/list', endpoint=self.object_list, methods=["GET"], name='membro_list'))
+        self.router.routes.append(Route(path='/membro/create', endpoint=self.object_create, methods=["GET","POST"], name='membro_create'))
+        self.router.routes.append(Route(path='/membro/details/{membro_id: int}', endpoint=self.object_details, methods=["GET"], name='membro_details'))
+        self.router.routes.append(Route(path='/membro/edit/{membro_id: int}', endpoint=self.object_edit, methods=["GET", "POST"], name='membro_edit'))
+        self.router.routes.append(Route(path='/membro/delete/{membro_id: int}', endpoint=self.object_delete, methods=["DELETE"], name='membro_delete'))
+
+        super().__init__('membro')
+
+    async def object_list(self, request: Request) -> Response:
+        #Rota para listar todos os membros [GET]
+        membro_controller: MembroController = MembroController(request)
+
+        return await super().object_list(objetc_controller=membro_controller)
+
+    async def object_delete(self, request: Request) -> Response:
+        #Rota para deletar um membro [DELETE]
+        membro_controller: MembroController = MembroController(request)
+
+        membro_id: int = request.path_params["membro_id"]
+
+        return await super().object_delete(object_controller=membro_controller, obj_id=membro_id)
+
+    async def object_create(self, request: Request) -> Response:
+        #Rota para carregar o template do formulário e criar um objeto [GET, POST]
+
+        membro_controller: MembroController = MembroController(request)
+
+        # Se request for GET
+        if request.method == 'GET':
+            # Adicionar o request no context
+            context = {"request": membro_controller.request, "ano": datetime.now().year}
+
+            return settings.TEMPLATES.TemplateResponse(f"admin/membro/create.html", context=context)
+
+        #se o request for POST
+        #Recebe os dados do form
+        form = await request.form()
+        dados: set = None
+
+        try:
+            await membro_controller.post_crud()
+        except ValueError as err:
+            nome: str = form.get('nome')
+            funcao: str = form.get('funcao')
+            dados = {"nome": nome, "funcao": funcao}
+            context = {
+                "request": request,
+                "ano": datetime.now().year,
+                "erro": err,
+                "objeto": dados
+            }
+            return settings.TEMPLATES.TemplateResponse("admin/membro/create.html", context=context)
+
+        return RedirectResponse(request.url_for("membro_list"), status_code=status.HTTP_201_CREATED)
+
+    async def object_edit(self, request: Request) -> Response:
+        # Rota para carregar o template do formulario de editao e atualizar um membro [GET, POST]
+        membro_controller: MembroController = MembroController(request)
+
+        membro_id: int = request.path_params["membro_id"]
+
+        # Se o request for GET
+        if request.method == 'GET':
+            return await super().object_details(object_controller=membro_controller, obj_id=membro_id)
+
+        # Se o request for post
+        membro = await membro_controller.get_one_crud(id_obj=membro_id)
+
+        if not membro:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+        # Recebe os dados do form
+        form = await request.form()
+        dados: set = None
+
+        try:
+            await membro_controller.post_crud(obj=membro)
+        except ValueError as err:
+            nome: str = form.get('nome')
+            funcao: str = form.get('funcao')
+            dados = {"id": membro_id, "nome": nome, "funcao": funcao}
+            context = {
+                "request": request,
+                "ano": datetime.now().year,
+                "error": err,
+                "dados": dados
+            }
+            return settings.TEMPLATES.TemplateResponse("admin/membro/edit.html", context=context)
+
+        return RedirectResponse(request.url_for("membro_list", status_code=status.HTTP_202_ACCEPTED))
+
+membro_admin = MembroAdmin()
+
+
